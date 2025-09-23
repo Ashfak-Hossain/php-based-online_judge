4d5ddd312b905e638947ff3fc94e607005b4683e
Berlin <evan1234.ek@gmail.com>
2025-09-23 19:40:17 +0600

    feat(debug): add debug information display

--- src/views/home.php
+++ src/views/home.php
@@ -10,3 +10,22 @@ $user = $_SESSION['user'] ?? null;
 <?php else: ?>
   <p>Please <a href="<?= BASE_URL ?>/login">log in</a> to access your account.</p>
 <?php endif; ?>
+
+<hr>
+
+<h3>Debug</h3>
+<ul>
+  <li>csrf token : <?= htmlspecialchars($_SESSION['csrf'] ?? '') ?></li>
+  <li>
+    <strong>Cookies</strong>
+    <pre>
+<?= htmlspecialchars(print_r($_COOKIE, true)) ?>
+    </pre>
+  </li>
+  <li>
+    <strong>Session</strong>
+    <pre>
+<?= htmlspecialchars(print_r($_SESSION, true)) ?>
+    </pre>
+  </li>
+</ul>
